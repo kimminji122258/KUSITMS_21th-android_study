13f91183cb1defb495454fd67f75249a99300324
kimminji122258 <kimminji122258@gmail.com>
2020-03-23 16:57:45 +0900

    second_study

--- 02_assignment/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 02_assignment/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,14 @@
-package com.example.layoutpractice;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frame);
-
-
+        setContentView(R.layout.activity_main);
     }
 }
