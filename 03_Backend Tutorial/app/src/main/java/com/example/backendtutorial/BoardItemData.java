46487847ba297d356d7bb93e60d905d8ee0f20f7
kimminji122258 <kimminji122258@gmail.com>
2020-03-26 20:59:19 +0900

    third study

---
+++ 03_Backend Tutorial/app/src/main/java/com/example/backendtutorial/BoardItemData.java
@@ -0,0 +1,20 @@
+package com.example.backendtutorial;
+
+public class BoardItemData {
+    private String title;
+    private String content;
+
+    public BoardItemData(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
