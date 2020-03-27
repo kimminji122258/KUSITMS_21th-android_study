2ddc361c1b26bafa5bcc5055b86fa3b0411f222d
kimminji122258 <kimminji122258@gmail.com>
2020-03-27 23:43:15 +0900

    remove 03_insta_assignment directory

--- 03_insta_backend_assignment/app/src/main/java/com/example/instalistview/FeedItemAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.example.instalistview;
-
-import android.widget.BaseAdapter;
-
-public class FeedItemAdapter extends BaseAdapter {
-}
--- 03_insta_backend_assignment/app/src/main/java/com/example/instalistview/FeedItemData.java
+++
@@ -1,4 +0,0 @@
-package com.example.instalistview;
-
-public class FeedItemData {
-}
