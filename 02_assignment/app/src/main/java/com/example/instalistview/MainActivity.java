aa0337869a0a25b41688272fc681d7f8f86cf8bf
kimminji122258 <kimminji122258@gmail.com>
2020-05-07 17:11:57 +0900

    0507 commit

--- 02_assignment/app/src/main/java/com/example/instalistview/MainActivity.java
+++ 02_assignment/app/src/main/java/com/example/instalistview/MainActivity.java
@@ -1,42 +1,80 @@
 package com.example.instalistview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
+    Fragment mainFragment = new MainFragment();
+    Fragment searchFragment = new SearchFragment();
+    Fragment likeFragment = new LikeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_view);
+        setContentView(R.layout.activity_navigation);
 
-        Button roading_btn = findViewById(R.id.roadingButton);
+        //ListView
+        ArrayList<FeedItemData> items = new ArrayList<>();
 
-        roading_btn.setOnClickListener(new View.OnClickListener(){
+        FeedItemAdapter feedItemAdapter = new FeedItemAdapter(getApplicationContext(),items);
+        ListView feedView = findViewById(R.id.feedView);
+        feedView.setAdapter(feedItemAdapter);
+
+        items.add(new FeedItemData(R.drawable.sample1, 3, "첫 번째 피드!"));
+        items.add(new FeedItemData(R.drawable.sample2, 4, "두 번째 피드!"));
+        items.add(new FeedItemData(R.drawable.sample3, 5, "세 번째 피드!"));
+
+        feedItemAdapter.notifyDataSetChanged();
+
+
+        fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, mainFragment).commit();
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottomNavView);
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
             @Override
-            public void onClick(View view)
-            {
-                //ListView
-                ArrayList<FeedItemData> items = new ArrayList<>();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment selectedFragment = null;
 
-                FeedItemAdapter feedItemAdapter = new FeedItemAdapter(getApplicationContext(),items);
-                ListView feedView = findViewById(R.id.feedView);
-                feedView.setAdapter(feedItemAdapter);
+                switch (item.getItemId()) {
+                    case R.id.homeNavButton:
+                        selectedFragment = mainFragment;
+                        break;
+                    case R.id.searchNavButton:
+                        selectedFragment = searchFragment;
+                        break;
+                    case R.id.likeNavButton:
+                        selectedFragment = likeFragment;
+                        break;
+                }
 
-                items.add(new FeedItemData(R.drawable.sample1, 3, "첫 번째 피드!"));
-                items.add(new FeedItemData(R.drawable.sample2, 4, "두 번째 피드!"));
-                items.add(new FeedItemData(R.drawable.sample3, 5, "세 번째 피드!"));
+                if (selectedFragment != null)
+                    fragmentTransaction.replace(R.id.frameLayout, selectedFragment).commit();
 
-                feedItemAdapter.notifyDataSetChanged();
+                return true;
             }
         });
+
+
 
 
     }
